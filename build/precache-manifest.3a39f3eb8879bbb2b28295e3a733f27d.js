735eda7f5bb4711b99bf01696af88dbc44aaec6f
Ervin <hamoydoerik@gmail.com>
2020-10-18 10:05:24 +0300

    Code refactoring

---
+++ build/precache-manifest.3a39f3eb8879bbb2b28295e3a733f27d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2f6bca1973274f3afbb",
+    "url": "/ErvinKhamoydo/got.git/static/css/main.8d943bb3.chunk.css"
+  },
+  {
+    "revision": "d2f6bca1973274f3afbb",
+    "url": "/ErvinKhamoydo/got.git/static/js/main.d2f6bca1.chunk.js"
+  },
+  {
+    "revision": "4f6034f4593d7337c75b",
+    "url": "/ErvinKhamoydo/got.git/static/css/1.18ae5ee3.chunk.css"
+  },
+  {
+    "revision": "4f6034f4593d7337c75b",
+    "url": "/ErvinKhamoydo/got.git/static/js/1.4f6034f4.chunk.js"
+  },
+  {
+    "revision": "222cc8f8f93b1eb6b9e5",
+    "url": "/ErvinKhamoydo/got.git/static/js/runtime~main.222cc8f8.js"
+  },
+  {
+    "revision": "ccd0c0854161a2e05ed92d4a38dded5f",
+    "url": "/ErvinKhamoydo/got.git/static/media/error.ccd0c085.jpg"
+  },
+  {
+    "revision": "d745ba9cefc03be8812adc41c86f450a",
+    "url": "/ErvinKhamoydo/got.git/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9312246519e2b0e4810908bf0a074fc.js"
+  "/ErvinKhamoydo/got.git/precache-manifest.3a39f3eb8879bbb2b28295e3a733f27d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ErvinKhamoydo/got.git/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
